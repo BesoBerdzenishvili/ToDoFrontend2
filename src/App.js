dbaf392e678b3d733729345722cd68a575504dd6
fliderman01 <fliderman01@gmail.com>
2022-12-19 15:44:33 +0400

    added fetch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import { useState, useEffect } from "react";
 function App() {
-  return <div>frontend 2</div>;
+  const [count, setCount] = useState(0);
+  const [workouts, setWorkouts] = useState([]);
+  const increase = () => {
+    setCount(count + 1);
+  };
+
+  useEffect(() => {
+    // fetch data
+    const dataFetch = async () => {
+      const data = await (await fetch("/api/tasks")).json();
+
+      // set state when the data received
+      setWorkouts(data);
+    };
+
+    dataFetch();
+  }, []);
+  console.log(workouts);
+
+  return (
+    <div>
+      {/* <button onClick={() => setShow(!show)}>data!</button> */}
+      <button onClick={increase}>Click!</button>
+      <p>{count}</p>
+      {workouts && workouts.map((i) => <p key={i._id}>{i.text}</p>)}
+    </div>
+  );
 }
 
 export default App;
